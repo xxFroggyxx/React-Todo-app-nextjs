92eadd5795f5b789bbba74ab03ba433e6d7fc18f
xxFroggyxx <xfroggyx@misiu.stargard.pl>
2022-01-30 03:06:51 +0100

    feat: add basics for delete system

--- pages/components/AppTodoList/List.js
+++ pages/components/AppTodoList/List.js
@@ -1,35 +1,41 @@
 import React from "react";
-import { StyledLi, StyledUl } from "./List.styles";
+import { DeleteAllButton, DeleteIcon, StyledLi, StyledUl } from "./List.styles";
 
 function List({ todos, filter, onChange }) {
   return (
-    <StyledUl
-      onChange={(e) => {
-        onChange(e.target.id);
-      }}
-    >
-      {todos
-        .filter((todo) => {
-          switch (filter) {
-            case "default":
-              return todo;
-            case "active":
-              return !todo.completed;
-            case "completed":
-              return todo.completed;
-          }
-        })
-        .map((todo) => (
-          <StyledLi key={todo.id} completed={todo.completed}>
-            <input
-              id={todo.id}
-              type="checkbox"
-              defaultChecked={todo.completed}
-            />
-            <label htmlFor={todo.id}>{todo.title}</label>
-          </StyledLi>
-        ))}
-    </StyledUl>
+    <>
+      <StyledUl
+        onChange={(e) => {
+          onChange(e.target.id);
+        }}
+      >
+        {todos
+          .filter((todo) => {
+            switch (filter) {
+              case "default":
+                return todo;
+              case "active":
+                return !todo.completed;
+              case "completed":
+                return todo.completed;
+            }
+          })
+          .map((todo) => (
+            <StyledLi key={todo.id} completed={todo.completed}>
+              <input
+                id={todo.id}
+                type="checkbox"
+                defaultChecked={todo.completed}
+              />
+              <label htmlFor={todo.id}>{todo.title}</label>
+              {filter === "completed" ? <DeleteIcon /> : null}
+            </StyledLi>
+          ))}
+      </StyledUl>
+      {filter === "completed" ? (
+        <DeleteAllButton>delete all</DeleteAllButton>
+      ) : null}
+    </>
   );
 }
 
--- pages/components/AppTodoList/List.styles.js
+++ pages/components/AppTodoList/List.styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { MdOutlineDelete } from "react-icons/md";
 
 export const StyledUl = styled.ul`
   list-style-type: none;
@@ -10,4 +11,25 @@ export const StyledLi = styled.li`
   font-weight: 500;
   font-size: 18px;
   margin-bottom: 1rem;
+  display: flex;
+  align-items: center;
+`;
+
+export const DeleteIcon = styled(MdOutlineDelete)`
+  font-size: 20px;
+  color: #bdbdbd;
+  margin-left: 1em;
+  cursor: pointer;
+`;
+
+export const DeleteAllButton = styled.button`
+  background: #eb5757;
+  border-radius: 4px;
+  width: 124px;
+  height: 40px;
+  border: 0;
+  outline: 0;
+  color: #fff;
+  float: right;
+  cursor: pointer;
 `;
