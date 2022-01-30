e0fbd8b29220c10a9b9a918ddf8da05dafbd254c
xxFroggyxx <xfroggyx@misiu.stargard.pl>
2022-01-30 02:35:01 +0100

    feat!: now can add tasks

--- pages/components/AppTodoAdd/Add.js
+++ pages/components/AppTodoAdd/Add.js
@@ -1,10 +1,35 @@
-import React from "react";
-import { AddButton, AddContainer } from "./Add.styles";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { AddContainer } from "./Add.styles";
 
-function Add() {
+function Add({ todos, setTodos }) {
+  const [content, setContent] = useState("");
+
+  const addNewTodo = (content) => {
+    setTodos([
+      ...todos,
+      {
+        id: uuidv4(),
+        title: content,
+        completed: false,
+      },
+    ]);
+  };
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    addNewTodo(content);
+    setContent("");
+  };
   return (
-    <AddContainer>
-      <input placeholder="add details" required />
+    <AddContainer onSubmit={handleFormSubmit}>
+      <input
+        placeholder="add details"
+        value={content}
+        onChange={(e) => {
+          setContent(e.target.value);
+        }}
+        required
+      />
       <button>Add</button>
     </AddContainer>
   );
